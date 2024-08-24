3f7a5a3ec62583dd9e8e2d2b7b8bcdeb3b95e769
Daniel Reza <92356684+DanielGuerrero2@users.noreply.github.com>
2024-08-23 22:49:53 -0600

    Add files via upload

---
+++ Eval_1_Variables/src/eval_2_variables/Eval_2_Variables.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eval_2_variables;
+
+/**
+ *
+ * @author danie
+ */
+public class Eval_2_Variables {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    String nombre;
+    String apellido;
+    String curp;
+    float promedio;
+    }
+    
+}
