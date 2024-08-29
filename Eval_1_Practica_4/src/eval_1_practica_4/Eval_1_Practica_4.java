0dd07d8e9335fa85418c74364876b14beb95b390
Daniel Reza <92356684+DanielGuerrero2@users.noreply.github.com>
2024-08-28 21:35:46 -0600

    Add files via upload

---
+++ Eval_1_Practica_4/src/eval_1_practica_4/Eval_1_Practica_4.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eval_1_practica_4;
+
+/**
+ *
+ * @author danie
+ */
+public class Eval_1_Practica_4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String marca;
+        String modelo;
+        double tamaño;
+        double precio;
+        String tecnologia;
+        
+       marca = "Asus";
+       modelo="Asus Rog";
+       tamaño=27;
+       tecnologia="Oled";
+       precio=9593.12;
+       
+       System.out.println(marca);
+       System.out.println(modelo);
+       System.out.println(tamaño); 
+       System.out.println(tecnologia);
+       System.out.println(precio);
+        
+    }
+    
+}
