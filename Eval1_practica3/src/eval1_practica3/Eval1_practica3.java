a5d8e429da9e18c6f0282adbea52467935b696dd
Daniel Reza <92356684+DanielGuerrero2@users.noreply.github.com>
2024-08-28 21:35:16 -0600

    Add files via upload

---
+++ Eval1_practica3/src/eval1_practica3/Eval1_practica3.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eval1_practica3;
+
+/**
+ *
+ * @author danie
+ */
+public class Eval1_practica3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+
+                // TODO code application logic here
+                //Automovil
+                String marca;
+                String modelo;
+                int año;
+                float precio;
+                
+                marca="Nissan";
+                modelo="GTR";
+                año=2008;
+                precio=648184128;
+                
+                System.out.println(marca);
+                System.out.println(modelo);
+                System.out.println(año);
+                System.out.println(precio);
+                
+                
+                
+                
+                
+                
+    }
+    
+}
