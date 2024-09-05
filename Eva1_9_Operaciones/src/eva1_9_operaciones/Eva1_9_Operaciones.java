8e03da39bb288e97d67f57b8f0e20238981f182b
Daniel Reza <92356684+DanielGuerrero2@users.noreply.github.com>
2024-09-04 18:45:06 -0600

    Add files via upload

---
+++ Eva1_9_Operaciones/src/eva1_9_operaciones/Eva1_9_Operaciones.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_9_operaciones;
+
+/**
+ *
+ * @author danie
+ */
+public class Eva1_9_Operaciones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x= 10;
+        int y= 5;
+
+        int suma , resta, mult, div, mod;
+
+        resta = x - y;
+        suma = x + y;
+        mult = x * y;
+        div = x / y;
+        mod = x % y;
+        System.out.println("Suma: " + suma);
+        System.out.println("Resta: " + resta);
+        System.out.println("Multiplicacion: " + mult);
+        System.out.println("Division: " + div);
+        System.out.println("Modulo: " + mod);
+        
+    }
+    
+}
