62c47232a88224e1d3b7baf41edc82b4eb7c849c
Daniel Reza <92356684+DanielGuerrero2@users.noreply.github.com>
2024-09-06 23:47:59 -0600

    Add files via upload

---
+++ Eva1_formulas_11/src/eva1_formulas_11/Eva1_formulas_11.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_formulas_11;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author danie
+ */
+public class Eva1_formulas_11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Pidele al usuario los datos para calcular la distancia
+        // en java
+        
+        //Declarar variables
+        double dDistancia, dVelocidad, dTiempo, dAceleracion;
+        //Pedir datos al usuario
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Introduce la velocidad: ");
+        dVelocidad = input.nextDouble();
+        System.out.println("Introduce el tiempo: ");
+        dTiempo = input.nextDouble();
+        //pide la aceleracion
+        System.out.println("Introduce la aceleracion: ");
+        dAceleracion = input.nextDouble();
+        dDistancia = dVelocidad * dTiempo + 0.5 * dAceleracion * dTiempo * dTiempo;
+        
+
+        
+      
+        
+
+
+        
+        
+        input.close();
+        System.out.println("La distancia recorrida es: " + dDistancia);
+
+
+
+        
+
+    }
+    
+}
