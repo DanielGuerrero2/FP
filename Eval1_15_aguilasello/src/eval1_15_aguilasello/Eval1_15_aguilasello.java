8a26d04aaad37de843cb3c9c6fb37b9bf716fa13
Daniel Reza <92356684+DanielGuerrero2@users.noreply.github.com>
2024-09-10 17:30:38 -0600

    Add files via upload

---
+++ Eval1_15_aguilasello/src/eval1_15_aguilasello/Eval1_15_aguilasello.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eval1_15_aguilasello;
+
+/**
+ *
+ * @author danie
+ */
+public class Eval1_15_aguilasello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int moneda = (int) (Math.random() * 2);
+    
+        if(moneda == 0){
+            System.out.println("aguila");
+        }else{
+            System.out.println("sello");
+        }
+
+    }
+    
+}
