5961496d4e186c83d53b73aedf26f6bc51f007f6
Daniel Reza <92356684+DanielGuerrero2@users.noreply.github.com>
2024-09-25 17:41:33 -0600

    Add files via upload

---
+++ Eva1_20_If_else/src/eva1_20_if_else/Eva1_20_If_else.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_20_if_else;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author danie
+ */
+public class Eva1_20_If_else {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int diaClase;
+        Scanner sc = new Scanner(System.in);
+        diaClase = sc.nextInt();
+        sc.close();
+     if(diaClase>0 && diaClase<8){
+        if (diaClase == 1){
+            System.out.println("es lunes y toca programacion");
+        }else if (diaClase==3){
+            System.out.println("es miercoles y programación");
+        } else if (diaClase==5){
+            {
+            
+            System.out.println("es viernes y toca programación");
+            }
+    
+        }else{
+            System.out.println("no hay clase de programación");
+        }
+
+    }else{
+        System.out.println("no es un dia valido");
+    }
+    
+         }
+
+
+    
+}
